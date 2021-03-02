c4e7d265d89ecf0ea4a806922e68a3dc715671e7
rodrigosaantos <contato.rodrigosaantos@gmail.com>
2021-03-01 23:51:22 -0300

    Leveling structure

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/RodrigoSaantos.png" alt="Rodrigo Saantos"/>
@@ -8,7 +11,7 @@ export function Profile() {
         <strong>Rodrigo Santos</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -16,7 +16,7 @@ interface ChallengeContextData {
   levelUp: () => void;
   startNewChallenge: () => void;
   resetChallenge: () => void;
-
+  completeChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -50,6 +50,25 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     setActiveChallenge(null);
   }
 
+  function completeChallenge() {
+    if (!activeChallenge) {
+      return;
+    }
+
+    const { amount } = activeChallenge;
+
+    let finalExperience = currentExperience + amount;
+
+    if (finalExperience >= experienceToNextLevel) {
+      finalExperience = finalExperience - experienceToNextLevel
+      levelUp()
+    }
+
+    setCurrentExperience(finalExperience);
+    setActiveChallenge(null);
+    setChallengesCompleted(challengesCompleted + 1);
+  }
+
   return (
     <ChallengesContext.Provider 
       value={{ 
@@ -57,10 +76,11 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         currentExperience, 
         challengesCompleted, 
         experienceToNextLevel,
+        activeChallenge, 
         levelUp,
         startNewChallenge,
-        activeChallenge,
         resetChallenge,
+        completeChallenge,
       }}
     >
       {children}
